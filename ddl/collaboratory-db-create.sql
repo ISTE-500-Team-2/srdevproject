649a442df580a862ced94af60167e5ec65980746
Tordwin <chenedwin6@gmail.com>
2026-08-28 22:31:18 -0400

    All tables created using sql script finish

--- ddl/collaboratory-db-create.sql
+++ ddl/collaboratory-db-create.sql
@@ -1,26 +1,26 @@
 -- Collaboratory 
 
 DROP TABLE IF EXISTS
-    user,
-    guest,
-    check_in,
+    reservation,
     day_pass,
+    payment,
+    check_in,
+    guest,
     user_waiver,
-    waiver,
-    user_role,
     user_certifications,
+    user_role,
     user_membership,
+    equipment,
+    waiver,
     certifications,
     role,
-    reservation,
-    payment,
-    equipment,
-    membership_tiers
+    membership_tiers,
+    user
 CASCADE;
 
 -- USER TABLE
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE user (
     userID INT PRIMARY KEY,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
@@ -32,39 +32,27 @@ CREATE TABLE IF NOT EXISTS user (
     registration_date DATETIME
 );
 
--- CHECK IN TABLE
-
-CREATE TABLE IF NOT EXISTS check_in (
-    checkInID INT PRIMARY KEY,
-    userID INT,
-    location VARCHAR(100) NOT NULL,
-    checkInTime DATETIME NOT NULL,
-    status VARCHAR(20),
-    statusDesc VARCHAR(255),
-    FOREIGN KEY (userID) REFERENCES user(userID)
-);
-
--- USER MEMBERSHIP TABLE
-
-CREATE TABLE IF NOT EXISTS user_membership (
-    membershipID INT PRIMARY KEY,
-    userID INT,
-    tierID INT,
-    startDate DATETIME NOT NULL,
-    endDate DATETIME NOT NULL,
-    status VARCHAR(20),
-    statusDesc VARCHAR(255),
-    FOREIGN KEY (userID) REFERENCES user(userID),
-    FOREIGN KEY (tierID) REFERENCES membership_tiers(tierID)
-);
-
 -- MEMBERSHIP TIERS TABLE
 
-CREATE TABLE IF NOT EXISTS membership_tiers (
+CREATE TABLE membership_tiers (
     tierID INT PRIMARY KEY,
     tierName VARCHAR(50) NOT NULL,
     tierPrice DECIMAL(10, 2) NOT NULL,
     allottedMonths INT
+);
+
+-- USER MEMBERSHIP TABLE
+
+CREATE TABLE user_membership (
+    membershipID INT PRIMARY KEY,
+    userID INT,
+    tierID INT,
+    startDate DATETIME,
+    end_date DATETIME,
+    status VARCHAR(20),
+    statusDesc VARCHAR(255),
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (tierID) REFERENCES membership_tiers(tierID)
 );
 
 -- PAYMENT TABLE
@@ -73,7 +61,7 @@ CREATE TABLE payment (
     paymentID INT PRIMARY KEY,
     membershipID INT,
     userID INT,
-    price INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     paymentStatus VARCHAR(20),
     paymentDate DATETIME,
     autoRenewStatus BOOLEAN DEFAULT FALSE,
@@ -95,3 +83,120 @@ CREATE TABLE day_pass (
     FOREIGN KEY (paymentID) REFERENCES payment(paymentID)
 )
 
+-- CHECK IN TABLE
+
+CREATE TABLE check_in (
+    checkInID INT PRIMARY KEY,
+    userID INT,
+    location VARCHAR(100) NOT NULL,
+    checkInTime DATETIME NOT NULL,
+    status VARCHAR(20),
+    statusDesc VARCHAR(255),
+    FOREIGN KEY (userID) REFERENCES user(userID)
+);
+
+-- GUEST TABLE
+
+CREATE TABLE guest (
+    guestID INT PRIMARY KEY,
+    hostID INT,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    visitDate DATETIME NOT NULL,
+    status VARCHAR(20),
+    statusDesc VARCHAR(255),
+    FOREIGN KEY (hostID) REFERENCES user(userID)
+);
+
+-- WAIVER TABLE
+
+CREATE TABLE waiver (
+    waiverID INT PRIMARY KEY,
+    name VARCHAR(100),
+    version VARCHAR(50),
+    description VARCHAR(255),
+    effectiveDate DATETIME
+);
+
+-- USER WAIVER TABLE
+
+CREATE TABLE user_waiver (
+    userWaiverID INT PRIMARY KEY,
+    userID INT,
+    waiverID INT,
+    signDate DATETIME,
+    approval BOOLEAN,
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (waiverID) REFERENCES waiver(waiverID)
+);
+
+-- CERTIFICATIONS TABLE
+
+CREATE TABLE certifications (
+    certID INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    effectiveDate DATETIME,
+    endDate DATETIME
+);
+
+-- USER CERTIFICATIONS TABLE
+
+CREATE TABLE user_certifications (
+    userCertID INT PRIMARY KEY,
+    userID INT,
+    certID INT,
+    renewalDate DATETIME,
+    status VARCHAR(20),
+    statusDesc VARCHAR(255),
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (certID) REFERENCES certifications(certID)
+);
+
+-- ROLE TABLE
+
+CREATE TABLE role (
+    roleID INT PRIMARY KEY,
+    role VARCHAR(50) NOT NULL,
+    description VARCHAR(255)
+);
+
+-- USER ROLE TABLE
+
+CREATE TABLE user_role (
+    rID INT PRIMARY KEY,
+    userID INT,
+    roleID INT,
+    assignedAt DATETIME,
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (roleID) REFERENCES role(roleID)
+);
+
+
+-- EQUIPMENT TABLE
+
+CREATE TABLE equipment (
+    equipmentID INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    status VARCHAR(20),
+    waiverRequired BOOLEAN
+);
+
+
+-- RESERVATION TABLE
+
+CREATE TABLE reservation (
+    reservationID INT PRIMARY KEY,
+    userID INT,
+    equipmentID INT,
+    waiverID INT,
+    location VARCHAR(100) NOT NULL,
+    startTime DATETIME NOT NULL,
+    endTime DATETIME NOT NULL,
+    status VARCHAR(20),
+    statusDesc VARCHAR(255),
+    FOREIGN KEY (userID) REFERENCES user(userID),
+    FOREIGN KEY (equipmentID) REFERENCES equipment(equipmentID),
+    FOREIGN KEY (waiverID) REFERENCES waiver(waiverID)
+);
