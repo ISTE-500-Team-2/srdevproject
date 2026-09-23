e3d44650e33fd94079cdf7d97e160d84a1140789
Tordwin <chenedwin6@gmail.com>
2026-09-23 16:35:27 -0400

    changed asshole flag to more appropriate name

--- database/migrations/010_user_profile_fields.sql
+++ database/migrations/010_user_profile_fields.sql
@@ -2,4 +2,4 @@
 -- customizable without baking one fixed form into the schema.
 ALTER TABLE "user" ADD COLUMN profile_address JSONB NOT NULL DEFAULT '{}'::jsonb;
 ALTER TABLE "user" ADD COLUMN contact_preferences JSONB NOT NULL DEFAULT '{}'::jsonb;
-ALTER TABLE "user" ADD COLUMN assholeflag BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "user" ADD COLUMN conductflag BOOLEAN NOT NULL DEFAULT false;
